2e772c346562dde6d4caf377ceac3e3521ec552d
QQ喵 <gqqnb2005@gmail.com>
2021-09-29 01:36:51 +0800

    健康吗截图.user.js

---
+++ 健康吗截图.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name        健康吗截图
+// @namespace   gqqnbig
+// @match       https://app.ssm.gov.mo/healthPHD/page/TradChin/ch8.html
+// @grant       none
+// @version     1.0
+// @author      gqqnbig
+// @description -
+// @grant       GM_notification
+// ==/UserScript==
+
+(function(){
+  
+$(function() {
+
+let control = GM_notification({text:'screenshot'});
+  
+});
+})();
